8c0b6a05751aae8894326856e8d7ff46ee5f111e
Geovane Ferreira <geovanevinicius89@gmail.com>
2021-10-28 21:56:57 -0300

    Initial commit

---
+++ POC01-DebuggingDemo/src/main.c
@@ -0,0 +1,50 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 2
+static uint8_t s_led_state = 0;
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    
+    while(true){
+        int val = 100;
+        for (int i = 10; i >= 0; i--) {
+            gpio_set_level(BLINK_GPIO, s_led_state);
+             s_led_state = !s_led_state;
+            printf("Hi %d\n", i);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            val--;
+        }
+    }
+}
